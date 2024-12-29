4c9792021b3dd2d5452bf6a2e0d985db1e782aab
Ryan Morshead <ryan.morshead@gmail.com>
2024-12-29 00:07:20 -0700

    add numpy

---
+++ src/lakery/extra/numpy.py
@@ -0,0 +1,56 @@
+from io import BytesIO
+from typing import Literal
+from typing import TypedDict
+
+import numpy as np
+
+from lakery.core.serializer import ValueDump
+from lakery.core.serializer import ValueSerializer
+
+
+class NpyDumpArgs(TypedDict, total=False):
+    """Arguments for dumping a NumPy array."""
+
+    allow_pickle: bool
+    fix_imports: bool
+
+
+class NpyLoadArgs(TypedDict, total=False):
+    """Arguments for loading a NumPy array."""
+
+    mmap_mode: Literal["r+", "r", "w+", "c"] | None
+    allow_pickle: bool
+    fix_imports: bool
+
+
+class NpySerializer(ValueSerializer[np.ndarray]):
+    """Serializer for Pandas DataFrames using Arrow."""
+
+    name = "lakery.numpy.npy"
+    version = 1
+    types = (np.ndarray,)
+
+    def __init__(
+        self,
+        *,
+        dump_args: NpyDumpArgs | None = None,
+        load_args: NpyLoadArgs | None = None,
+    ) -> None:
+        self._dump_args = dump_args or {}
+        self._load_args = load_args or {}
+
+    def dump_value(self, value: np.ndarray, /) -> ValueDump:
+        """Serialize the given DataFrame."""
+        buffer = BytesIO()
+        np.save(buffer, value, **self._dump_args)
+        return {
+            "content_encoding": None,
+            "content_type": "application/x-npy.v3",
+            "serializer_name": self.name,
+            "serializer_version": self.version,
+            "value": buffer.getvalue(),
+        }
+
+    def load_value(self, dump: ValueDump, /) -> np.ndarray:
+        """Deserialize the given DataFrame."""
+        return np.load(BytesIO(dump["value"]), **self._load_args)
---
+++ tests/test_lakery/test_extra/test_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from lakery.extra.numpy import NpySerializer
+from tests.serializer_utils import make_value_serializer_test
+
+
+def _assert_equal(a: np.ndarray, b: np.ndarray):
+    np.testing.assert_array_equal(a, b)
+
+
+test_npy_value_serializer = make_value_serializer_test(
+    NpySerializer(),
+    np.array([1, 2, 3]),
+    assertion=_assert_equal,
+)
