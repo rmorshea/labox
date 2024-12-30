ce907eb10c614e32de62212ceacaba5d90da5dc3
Ryan Morshead <ryan.morshead@gmail.com>
2024-12-30 13:03:18 -0700

    add future annotations

--- src/lakery/core/context.py
+++ src/lakery/core/context.py
@@ -1,5 +1,7 @@
-from collections.abc import Iterator
+from __future__ import annotations
+
 from contextlib import contextmanager
+from typing import TYPE_CHECKING
 from typing import Any
 from typing import NewType
 
@@ -8,6 +10,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from lakery.core.serializer import SerializerRegistry
 from lakery.core.storage import StorageRegistry
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 DatabaseSession = NewType("DatabaseSession", AsyncSession)
 """A type alias for an lakery database session."""
